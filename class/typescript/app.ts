d4f7252e4669fc74f925196298178fa5d89c47a8
Marcio de Jesus <mjesus@lbv.org.br>
2019-07-01 10:46:16 -0300

    Seçao 5 - inciando app1

--- class/typescript/app.ts
+++
@@ -1,6 +0,0 @@
-let ola = (nome: string, sobrenome: string) => {
-  console.log("Olá " + nome + sobrenome)
-}
-
-ola("Jorge", "Matheus")
-ola("Fulano", "Ciclado")
--- class/typescript/dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var ola = function (nome, sobrenome) {
-    console.log("Olá " + nome + sobrenome);
-};
-ola("Jorge", "Matheus");
-ola("Fulano", "Ciclado");
