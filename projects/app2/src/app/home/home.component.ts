f472bc3816a5d59bd8c95109b4b446dad79839b8
Marcio de Jesus <mjesus@lbv.org.br>
2019-07-19 15:50:58 -0300

    Inicio do conceito de serviços - criação de um modelo para expor no home component Sessão 8 - 92-96

--- projects/app2/src/app/home/home.component.ts
+++ projects/app2/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    console.log(this.ofertas = this.ofertasService.getOfertas())
   }
 
 }
