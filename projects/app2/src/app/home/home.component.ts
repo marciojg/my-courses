2738ed6df97eea9250a5b4d3d6c19f406fc582b8
Marcio de Jesus <mjesus@lbv.org.br>
2019-07-29 10:40:34 -0300

    exibindo home componente view dinamicamente Sessão 8 - 96-97

--- projects/app2/src/app/home/home.component.ts
+++ projects/app2/src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log(this.ofertas = this.ofertasService.getOfertas())
+    this.ofertas = this.ofertasService.getOfertas()
   }
 
 }
