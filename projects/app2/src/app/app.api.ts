2206c3c746133f049e48211edeff0579118c3b9f
Marcio de Jesus <mjesus@lbv.org.br>
2019-08-05 10:24:51 -0300

    tratando erro template oferta e refatorando acesso a url api 126-127-128

---
+++ projects/app2/src/app/app.api.ts
@@ -0,0 +1,3 @@
+const API_URL = 'http://localhost:4000'
+
+export const API_URL_OFERTAS = `${API_URL}/ofertas`
--- projects/app2/src/app/ofertas.service.ts
+++ projects/app2/src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Oferta } from './shared/oferta.model'
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
+import { API_URL_OFERTAS } from './app.api';
 
 @Injectable()
 export class OfertasService {
@@ -8,20 +9,20 @@ export class OfertasService {
   constructor(private http: HttpClient) {}
 
   public getOfertas(): Promise<Oferta[]> {
-    return this.http.get('http://localhost:4000/ofertas?destaque=true')
+    return this.http.get(`${API_URL_OFERTAS}?destaque=true`)
                .toPromise()
                .then((resposta: any) => resposta)
     // No curso, o parametro resposta precisa ser convertido para json, usando resposta.json()
   }
 
   public getOfertasPorCategoria(categoria: string) {
-    return this.http.get(`http://localhost:4000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${API_URL_OFERTAS}?categoria=${categoria}`)
                .toPromise()
                .then((resposta: any) => resposta)
   }
 
   public getOfertaPorId(id: number) {
-    return this.http.get(`http://localhost:4000/ofertas?id=${id}`)
+    return this.http.get(`${API_URL_OFERTAS}?id=${id}`)
                .toPromise()
                .then((resposta: any) => resposta[0])
   }
