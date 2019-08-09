c62752ea353b7c924f567f03a788864803b6c9ae
= <=>
2019-08-09 18:01:33 -0300

    usando subject com switchmap e deounceTime para somente fazer a requisição http depois de 1 segundo

--- projects/app2/src/app/topo/topo.component.ts
+++ projects/app2/src/app/topo/topo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Subject, Observable } from 'rxjs';
+import { switchMap, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -10,17 +12,26 @@ import { Oferta } from '../shared/oferta.model';
 })
 export class TopoComponent implements OnInit {
 
+  public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
+
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+      .pipe(
+        debounceTime(1000),
+        switchMap((termo: string) => {
+          return this.ofertasService.pesquisaOfertas(termo)
+        })
+      )
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => console.log('retornoou aqui maluco', ofertas))
   }
 
-  public pesquisa(termo: string): void {
-    this.ofertasService.pesquisaOfertas(termo)
-                       .subscribe(
-                         (ofertas: Oferta[]) => console.log(ofertas),
-                         (erro: any) => console.log('errrrou', erro)
-                        )
+  public pesquisa(termoDaPesquisa: string): void {
+    console.log('evento do keyup', termoDaPesquisa)
+    this.subjectPesquisa.next(termoDaPesquisa)
   }
 
 }
