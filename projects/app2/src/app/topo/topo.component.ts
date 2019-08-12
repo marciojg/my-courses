bd0ffb31a5c52e50c1b767eda6a36b01f3032964
= <=>
2019-08-12 17:30:30 -0300

    tratabdo requisições duplicadas com api, tratamento de erro e requisição *.

--- projects/app2/src/app/topo/topo.component.ts
+++ projects/app2/src/app/topo/topo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Subject, Observable } from 'rxjs';
-import { switchMap, debounceTime } from 'rxjs/operators';
+import { Subject, Observable, of } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
@@ -13,6 +13,7 @@ import { switchMap, debounceTime } from 'rxjs/operators';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
   private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
@@ -21,12 +22,27 @@ export class TopoComponent implements OnInit {
     this.ofertas = this.subjectPesquisa
       .pipe(
         debounceTime(1000),
+        distinctUntilChanged(),
         switchMap((termo: string) => {
+          console.log('requisição http para a api')
+
+          if(termo.trim() === '') {
+            console.log('enttrou no if')
+            return of([])
+          }
+
           return this.ofertasService.pesquisaOfertas(termo)
+        }),
+        catchError((erro: any) => {
+          console.log('Erro tratado pelo catch Error', erro)
+          return of([])
         })
       )
 
-    this.ofertas.subscribe((ofertas: Oferta[]) => console.log('retornoou aqui maluco', ofertas))
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      console.log('retornoou aqui maluco', ofertas)
+      this.ofertas2 = ofertas
+    })
   }
 
   public pesquisa(termoDaPesquisa: string): void {
