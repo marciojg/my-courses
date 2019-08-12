a30b8e7a5abf415928c6b71ef567f556ff8e5ff4
= <=>
2019-08-12 17:57:57 -0300

    traduzir moeda para pt

--- projects/app2/src/app/app.module.ts
+++ projects/app2/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
 
 import { RouterModule } from '@angular/router'
 import { ROUTES } from './app.routes';
@@ -16,6 +17,10 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
