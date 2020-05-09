ec3c64a699f072cd6c89cc1b5f505d3f115c7eb3
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-05-09 12:34:11 -0300

    Minify styles.css in env Production * Installed and Settinged two plugins to Process and minify css files Neste capítulo vimos:
    Como utilizar o npm para gerenciar nossas dependências frontend
    Como o Webpack lida com as dependências em node_modules adicionado-as no bundle da aplicação.
    O papel de loaders
    Que o padrão é adicionar no bundle scripts e CSS's
    Que podemos separar CSS's do bundle criado e importá-los através da tag link através do módulo extract-text-webpack-plugin.
    A utilizar o plugin optimize-css-assets-webpack-plugin para minificar CSS's importados se adicionados no style.css.

--- webpack/client/webpack.config.js
+++ webpack/client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const plugins = [
   new extractTextPlugin("styles.css")
@@ -8,6 +9,16 @@ const plugins = [
 
 if(process.env.NODE_ENV == 'production') {
   plugins.push(new babiliPlugin());
+
+  plugins.push(new optimizeCSSAssetsPlugin({
+    cssProcessor: require('cssnano'),
+    cssProcessorOptions: {
+        discardComments: {
+            removeAll: true
+        }
+    },
+    canPrint: true
+  }));
 }
 
 // CARREGA DA DIREITA PARA ESQUERDA
