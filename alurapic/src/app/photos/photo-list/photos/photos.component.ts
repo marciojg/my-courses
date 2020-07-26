d508c3b27564ba702c4ddcab10f1446f07ddfb0b
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-07-26 15:01:43 -0300

    Part 1 - Class - 05:   - Implementando a lógica do Grid   - Change Detection
    - Changing grid logic to show photos
    - Implement logic to detect changes in an inbound property (@imput())

--- alurapic/src/app/photos/photo-list/photos/photos.component.ts
+++ alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,13 +6,37 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  /*
+  Vamos testar isso implementando if para o caso de haver mudanças especificamente
+  na inbound property photos e, caso positivo, executaremos this.groupColumns()
+  passando os novos dados das imagens.
+  Testamos com photos pois poderemos ter várias propriedades,
+  porém apenas uma delas sofrer alteração.
+  É necessário testar cada propriedade da inbound property.
+
+  **SimpleChanges**
+  Um objeto do tipo SimpleChanges possui uma propriedade de mesmo
+  nome da inbound property que sofreu mudança.
+  */
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
   }
 
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
+  }
 }
