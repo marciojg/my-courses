585ae1c5bb3da5669790781040ba4dd41d1084e1
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-07-26 18:36:41 -0300

    Part 1 - Class - 06:   - RxJS e seu Subject
    - Forcing wait of 300ms to make the search/filter of photos, using the Debounce Pattern with help of Subject and debounceTime of rxjs lib

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
 
 @Component({
@@ -9,10 +10,11 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photoList: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
@@ -20,5 +22,26 @@ export class PhotoListComponent implements OnInit {
 
   ngOnInit(): void {
     this.photoList = this.activatedRoute.snapshot.data.photoList;
+
+    /*
+    Por meio de next(), é possível emitirmos um valor para Subject,
+    que acessamos caso o tenhamos inscrito. Diferentemente do Observable, com o qual podemos inscrever e obter valores,
+    com o Subject podemos, além disso, emitir um valor e escutá-lo.
+    A grande sacada é que, com esta alteração chamada de Lettable operators no RxJS,
+    por usarmos o debounceTime, quando emitimos um valor no evento keyup,
+    todas as emissões serão ignoradas, sendo consideradas após 300ms.
+    E é isso que será repassado ao subscribe().
+    O Observable é engenhoso para lidar com situações deste tipo, com fluxos e eventos,
+    e colocamos threshold, um Pipe no que chamamos de debounce,
+    para limitar a quantidade de operações.
+     */
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    /* Como a inscrição não tem "fim", ou seja, não pode ser concluída é necessário desinscrever quando o componente é destruido, ou seja, o usuário sai da página e não precisa mais do filtro ativo. */
+    this.debounce.unsubscribe();
   }
 }
