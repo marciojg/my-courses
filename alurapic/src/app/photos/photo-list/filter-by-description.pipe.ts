7ccd5e17fa6f195785275a93b7c868491c51601d
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-07-26 17:12:57 -0300

    Part 1 - Class - 06:   - Binding de eventos   - Pipe e implementação
    - Using Event binding to get the word than user want to use to the photos filter
    - Creating and using Pipe with the objective to be a filter

---
+++ alurapic/src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Photo } from '../photo/photo';
+
+@Pipe({pure: false, name: 'filterByDescription' })
+export class FilterByDescriptionPipe implements PipeTransform {
+
+  transform(photos: Photo[], descriptionQuery: string) {
+    descriptionQuery = descriptionQuery
+        .trim()
+        .toLowerCase();
+
+    if(descriptionQuery) {
+      return photos.filter(photo =>
+        photo.description.toLowerCase().includes(descriptionQuery)
+      );
+    } else {
+      return photos;
+    }
+  }
+}
