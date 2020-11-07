870a0a27edcee993624483a7f3e4126dd6843367
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-07 11:12:53 -0300

    Part 2 - Class - 01:   - Create an Authentication service to login user   - On Submit form send POST request do backend   - Clear the form then request return success and show the alert error when not

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -7,12 +8,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+    ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: [ null, Validators.required ],
       password: [ null, Validators.required ]
     })
+  }
+
+  login() {
+    const { userName, password } = this.loginForm.value
+
+    this.authService.authenticate(userName, password).
+        subscribe(
+          () => console.log('autenticado'),
+          error => {
+            console.log(error);
+            this.loginForm.reset();
+            alert('Invalid user name or password');
+          }
+        )
   }
 }
