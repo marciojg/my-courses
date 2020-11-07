24d08ec2a0f444f713ffb66b090f9d8fc4df6851
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-07 11:34:49 -0300

    Part 2 - Class - 01:   - Redirect the user to your photos page when login is successful
      PS. The import of RouterModule in HomeModule is optinal, because the module AppRouterModule are the same list of importers of AppModule and AppRouterModule already export RouterModule

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SignInComponent } from './signin/signin.component';
 
@@ -8,7 +9,8 @@ import { SignInComponent } from './signin/signin.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    VMessageModule
+    VMessageModule,
+    RouterModule
   ],
   declarations: [ SignInComponent ]
 })
--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
@@ -10,7 +11,8 @@ export class SignInComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
     ) {}
 
   ngOnInit(): void {
@@ -25,7 +27,7 @@ export class SignInComponent implements OnInit {
 
     this.authService.authenticate(userName, password).
         subscribe(
-          () => console.log('autenticado'),
+          () => this.router.navigate(['user', userName]),
           error => {
             console.log(error);
             this.loginForm.reset();
