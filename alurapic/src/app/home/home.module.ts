1b8788ad3b232534d2e05e3ec0c932c35b2ecd71
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-07 10:32:12 -0300

    Part 2 - Class - 01:   - Implements initials validations in form using `ReactFormsModule` of `@angular/forms`   - Show validation errors to user   - Use safe operator (object?.property) to avoid rise errors like this: `ERROR TypeError: Cannot read property 'required' of null`   - Enable submit button only when loginForm is valid

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,7 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   declarations: [ SignInComponent ]
 })
 export class HomeModule {}
