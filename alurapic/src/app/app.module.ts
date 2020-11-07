920c21c64dae5cff95f64c83d5d137d5f212f27d
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-07 10:00:57 -0300

    Part 2 - Class - 01:       - Create the SignIn component       - Create the Home module to export/declare the authentication components

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
+import { PhotosModule } from './photos/photos.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,8 @@ import { ErrorsModule } from './errors/errors.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
