01048c43a216f577ead4d75542337ed720e0487d
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-16 20:49:27 -0300

    Part 2 - Class - 03:   - Export name of user exported by UserService in HeaderComponent   - Change type Subject of `userSubject` to BehaviorSubject to avoid problems with initialization order between service and component. Showing name of user after the HeaderComponente be reloaded after the user reload the user page.

--- alurapic/src/app/core/user/user.service.ts
+++ alurapic/src/app/core/user/user.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { TokenService } from '../token/token.service';
 import { User } from './user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
 
-  private userSubject = new Subject<User>();
+  private userSubject = new BehaviorSubject<User>(null);
 
   constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() && this.decodeAndNotify();
