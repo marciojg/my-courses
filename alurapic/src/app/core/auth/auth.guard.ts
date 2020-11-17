a2e2f67ddcc7e4eb1019c7610845516b4c762db9
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-11-16 21:26:12 -0300

    Part 2 - Class - 03:   - Not allow access on login page when the user is logged   - When the user is logged in and try access the login page, he must be redirected to your page of photos

---
+++ alurapic/src/app/core/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private userUservice: UserService,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    if(this.userUservice.isLogged()) {
+      this.router.navigate(['user', this.userUservice.getUserName()]);
+      return false;
+    }
+
+    return true;
+  }
+}
--- alurapic/src/app/core/user/user.service.ts
+++ alurapic/src/app/core/user/user.service.ts
@@ -8,9 +8,10 @@ import { User } from './user';
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
-    this.tokenService.hasToken() && this.decodeAndNotify();
+    this.isLogged() && this.decodeAndNotify();
   }
 
   setToken(token: string) {
@@ -27,9 +28,18 @@ export class UserService {
     this.userSubject.next(null);
   }
 
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName() {
+    return this.userName;
+  }
+
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
 
     this.userSubject.next(user);
   }
