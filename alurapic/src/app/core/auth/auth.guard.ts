d22c3132bfe62978dc1ee0a91f410b0e5cd5c421
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-19 10:32:22 -0300

    Part 3 - Class - 02:   - Permit only authenticated user access PhotoFormComponent

--- alurapic/src/app/core/auth/auth.guard.ts
+++ alurapic/src/app/core/auth/auth.guard.ts
@@ -3,20 +3,23 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 import { Observable } from 'rxjs';
 import { UserService } from '../user/user.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private userUservice: UserService,
+    private userService: UserService,
     private router: Router
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-    if(this.userUservice.isLogged()) {
-      this.router.navigate(['user', this.userUservice.getUserName()]);
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+
+    if(!this.userService.isLogged()){
+      this.router.navigate(['']);
       return false;
     }
-
     return true;
   }
 }
