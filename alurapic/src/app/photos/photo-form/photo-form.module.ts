075b85bf626779390e6f079fd58c18b1182c857e
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-19 10:16:35 -0300

    Part 3 - Class - 02:   - Craete a directive to forcing the click of select image when the PhotoFormComponet is load

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click-module';
 import { PhotoModule } from '../photo/photo.module';
 import { PhotoFormComponent } from './photo-form.component';
 
@@ -16,7 +17,8 @@ import { PhotoFormComponent } from './photo-form.component';
     FormsModule,
     VMessageModule,
     RouterModule,
-    PhotoModule
+    PhotoModule,
+    ImmediateClickModule
   ]
 })
 export class PhotoFormModule {}
