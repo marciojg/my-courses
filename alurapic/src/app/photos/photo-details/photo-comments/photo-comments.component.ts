7454362179353ec990b6d58c46f69c1ea9f29701
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-21 21:28:18 -0300

    Part 3 - Class - 04:   - Submitting a comment to backend api   - Automatically reload the list of comments after submitting a comment

--- alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from "rxjs";
+import { switchMap, tap } from 'rxjs/operators';
 import { PhotoComment } from "../../photo/photo-comment";
 import { PhotoService } from "../../photo/photo.service";
 
@@ -26,5 +27,20 @@ export class PhotoCommentsComponent implements OnInit {
       this.commentForm = this.formBuilder.group({
         comment: ['', Validators.maxLength(300)]
       });
+    }
+
+    save() {
+      const comment = this.commentForm.get('comment').value as string;
+
+      this.comments$ = this.photoService
+          .addComment(this.photoId, comment)
+          .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+          // Antes do getComments() retornar o observable, conseguimos executar um código arbitrário, graças ao tap.
+          .pipe(tap(
+            () => {
+              this.commentForm.reset();
+              alert('Comentário adicionado com sucesso');
+            }
+          ))
     }
 }
