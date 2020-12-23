d03cb0ffd203129d509052c5fe78f071d57bc607
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-23 00:31:16 -0300

    Part 3 - Class - 08:   - Differ deploy environments

--- alurapic/src/app/photos/photo/photo.component.ts
+++ alurapic/src/app/photos/photo/photo.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
-const CLOUD = 'http://localhost:3000/imgs/';
+const CLOUD = `${environment.apiUrl}/imgs/`;
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
