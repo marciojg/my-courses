5b1f9039826fff9a0dfeaa44eefb68413ad15b46
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-22 23:04:39 -0300

    Part 3 - Class - 06:   - Show Created Alert on delete photo

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -13,7 +14,8 @@ export class PhotoDetailsComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private photoService: PhotoService,
-    private router: Router
+    private router: Router,
+    private alertService: AlertService
   ) {}
 
   ngOnInit(): void {
@@ -24,6 +26,15 @@ export class PhotoDetailsComponent implements OnInit {
   remove() {
     this.photoService
         .removePhoto(this.photoId)
-        .subscribe(() => this.router.navigate(['']));
-}
+        .subscribe(
+        () => {
+          this.alertService.success('Photo removed');
+          this.router.navigate(['']);
+        },
+        err => {
+          console.log(err);
+          this.alertService.warning('Could not delete the photo!');
+        });
+
+  }
 }
