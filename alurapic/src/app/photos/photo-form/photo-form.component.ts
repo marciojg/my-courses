20738e026daf37b072aff385199a465571261b80
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-29 10:59:03 -0300

    Part 4 - Class - 02:   - Show progress bar to user when a photo is loading/uploading   - Using default returns of HttpClient to show progress bar   - Using the operator `finalize` on pipe

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -35,11 +38,26 @@ export class PhotoFormComponent implements OnInit {
   upload() {
     const { description, allowComments } = this.photoForm.getRawValue();
 
-    this.photoService.upload(description, allowComments, this.file)
-      .subscribe(() => {
-        this.alertService.success('Upload Complete', true);
-        this.router.navigate(['/user', this.userService.getUserName()]);
-      })
+    this.photoService
+        .upload(description, allowComments, this.file)
+        /* dispara o evento dentro do finalize independente do resultado do subscribe (sucesso ou falha) */
+        .pipe(finalize(() => {
+          this.router.navigate(['/user', this.userService.getUserName()]);
+        }))
+        .subscribe(
+          (event: HttpEvent<any>) => {
+            if (event.type == HttpEventType.UploadProgress) {
+              this.percentDone = Math.round(100 * event.loaded / event.total);
+              // Outra opção é usar event instanceof  HttpResponse
+            } else if (event.type == HttpEventType.Response) {
+              this.alertService.success('Upload complete', true);
+            }
+          },
+          err => {
+            console.log(err);
+            this.alertService.danger('Upload error!', true);
+          }
+        );
   }
 
   /*
