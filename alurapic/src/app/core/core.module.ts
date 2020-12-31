21b6ee24e5ff0432110e646f1f435885d248e57b
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-31 09:47:15 -0300

    Part 4 - Class - 04:   - Create and show menu bar

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -21,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
     CommonModule,
     RouterModule,
     AlertModule,
-    LoadingModule
+    LoadingModule,
+    MenuModule
   ],
   providers: [
     {
