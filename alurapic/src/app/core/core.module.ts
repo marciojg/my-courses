85dc18f206901a0f9d82f23ef5cc4b35c19c38b0
Marcio de Jesus <marciodejesusrj@gmail.com>
2020-12-31 10:03:23 -0300

    Part 4 - Class - 04:   - Show menu item only when user is logged

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
 import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +24,8 @@ import { HeaderComponent } from './header/header.component';
     RouterModule,
     AlertModule,
     LoadingModule,
-    MenuModule
+    MenuModule,
+    ShowIfLoggedModule
   ],
   providers: [
     {
--- alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -4,14 +4,29 @@ import { UserService } from 'src/app/core/user/user.service';
 @Directive({ selector: '[showIfLogged]' })
 export class ShowIfLoggedDirective implements OnInit {
 
+  currentDisplay: string;
+
   constructor(
       private element: ElementRef<any>,
       private renderer: Renderer2,
       private userService: UserService
   )   {}
 
+  /*
+  A questão é que nosso menu faz parte do header da aplicação, e ele nunca é recarregado.
+  Isso significa que o último estado do menu sempre será o mesmo. Para resolver esse problema,
+  acrescentaremos algumas modificações em show-if-logged.directive.ts, tornando seu código um pouco
+  mais complexo.
+  */
   ngOnInit(): void {
-    !this.userService.isLogged()
-    && this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+    this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+    this.userService.getUser().subscribe(user => {
+      if(user) {
+        this.renderer.setStyle(this.element.nativeElement, 'display', this.currentDisplay);
+      } else {
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+      }
+   });
   }
 }
