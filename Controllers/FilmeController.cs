9d2496a4a9ba3db7270b7d324327a0e25cce5280
Marcio de Jesus <marciodejesusrj@gmail.com>
2021-08-26 22:13:18 -0300

    Como abrir uma conexão entre a API e o banco de dados. Como gerar migrations e mapear nosso objeto no banco de dados. Como utilizar o DbContext para fazer operações no banco. Como atualizar um recurso no sistema com o verbo PUT. Como deletar um recurso no sistema com o verbo DELETE.

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FilmesAPI.Data;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,14 +12,18 @@ namespace FilmesAPI.Controllers
 	[Route("[controller]")]
 	public class FilmeController : ControllerBase
 	{
-		private static List<Filme> filmes = new List<Filme>();
-		private static int Id = 1;
+		private FilmeContext _context;
+
+		public FilmeController(FilmeContext context)
+		{
+			_context = context;
+		}
 
 		[HttpPost]
 		public IActionResult AdicionaFilme([FromBody] Filme filme)
 		{
-			filme.Id = Id++;
-			filmes.Add(filme);
+			_context.Add(filme);
+			_context.SaveChanges();
 			return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
 
 		}
@@ -26,19 +31,55 @@ namespace FilmesAPI.Controllers
 		[HttpGet]
 		public IActionResult RecuperaFilmes()
 		{
-			return Ok(filmes);
+			return Ok(_context.Filmes);
 		}
 
 		[HttpGet("{id}")]
 		public IActionResult RecuperaFilmesPorId(int id)
 		{
-			Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+			Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
 			if (filme != null) {
 				return Ok(filme);
 			}
 
 			return NotFound();
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult AtualizaFilme(int id, [FromBody] Filme newFilme) 
+		{
+			Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+			if (filme == null)
+			{
+				return Ok(filme);
+			}
+
+			filme.Titulo = newFilme.Titulo;
+			filme.Genero = newFilme.Genero;
+			filme.Diretor = newFilme.Diretor;
+			filme.Duracao = newFilme.Duracao;
+			
+			_context.SaveChanges();
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult RemoveFilme(int id)
+		{
+			Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+			if (filme == null)
+			{
+				return Ok(filme);
+			}
+
+			_context.Remove(filme);
+			_context.SaveChanges();
+
+			return NoContent();
 		}
 	}
 }
---
+++ Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+	public class FilmeContext : DbContext
+	{
+		public FilmeContext(DbContextOptions<FilmeContext> opt) : base (opt)
+		{
+
+		}
+
+		public DbSet<Filme> Filmes { get; set; }
+	}
+}
